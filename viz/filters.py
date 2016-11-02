1aeba4bba06ed682325c517f4fc4a8b553657878
Daniel Worrall <d.worrall@cs.ucl.ac.uk>
2016-11-02 19:14:03 +0000

    alterations to figs

--- viz/filters.py
+++ viz/filters.py
@@ -5,51 +5,66 @@ import sys
 import time
 
 import numpy as np
+import seaborn as sns
 
 from matplotlib import pyplot as plt
 
 def main():
-	k = 20.
-	phi = 0.*np.pi
-	G, A = get_complex_basis(k=k)
-
-	X = np.cos(A-phi)
-	Y = np.sin(A-phi)
+	m = 2
+	s = 1.
+	k = 200
+	x = np.linspace(-1,1,num=k)
+	y = np.linspace(-1,1,num=k)
+	X, Y = np.meshgrid(x, y)
+	Theta = np.arctan2(Y, X)
+	d = np.sqrt(x**2 + y**2)
+	R = np.exp(-d**2/s)
 	
-	lin = np.linspace((1.-k)/2., (k-1.)/2., k)/(k/5.)
+	Wr = R*np.cos(m*Theta)
+	Wi = R*np.sin(m*Theta)
+	
 	plt.figure(1)
-	plt.quiver(X,Y)
-	plt.imshow(G)
-	
+	plt.imshow(Wr, interpolation='nearest', cmap='Blues')
+	plt.axis('off')
+	plt.figure(2)
+	plt.imshow(Wi, interpolation='nearest', cmap='Reds')
+	plt.axis('off')
 	plt.show()
-
-def get_basis(k=3,n=2):
-    """Return a tensor of steerable filter bases"""
-    lin = np.linspace((1.-k)/2., (k-1.)/2., k)/(k/5.)
-    x, y = np.meshgrid(lin, lin)
-    gdx = gaussian_derivative(x, y, x)
-    gdy = gaussian_derivative(x, y, y)
-    G0 = np.reshape(gdx/np.sqrt(np.sum(gdx**2)), [k,k,1])
-    G1 = np.reshape(gdy/np.sqrt(np.sum(gdy**2)), [k,k,1])
-    return np.concatenate([G0,G1], axis=2)
-
-def get_complex_basis(k=3,n=2):
-	"""Return a tensor of steerable filter bases"""
-	lin = np.linspace((1.-k)/2., (k-1.)/2., k)/(k/5.)
-	x, y = np.meshgrid(lin, lin)
-	G = gaussian_derivative(x, y, x)
-	A = np.arctan2(y, x)
-	return G, A
-
-def gaussian_derivative(x,y,direction):
-	r2 = x**2 + y**2
-	return -2*np.sqrt(r2)*np.exp(-r2)
 
 def gaussian(x,y):
     return np.exp(-(x**2 + y**2))
 
+def plot_sinusoid():
+	x = np.linspace(0,2*np.pi,num=360)
+	plt.figure(1)
+	plt.plot(np.cos(x), 'b', linewidth=5)
+	plt.axis('off')
+	plt.ylim([-1.05,1.05])
+	
+	plt.figure(2)
+	plt.plot(np.sin(x), 'r', linewidth=5)
+	plt.axis('off')
+	plt.ylim([-1.05,1.05])
+	
+	plt.figure(3)
+	plt.plot(np.cos(2*x), 'b', linewidth=5)
+	plt.axis('off')
+	plt.ylim([-1.05,1.05])
+	
+	plt.figure(4)
+	plt.plot(np.sin(2*x), 'r', linewidth=5)
+	plt.axis('off')
+	plt.ylim([-1.05,1.05])
+	plt.show()
+
+def get_colors():
+	for i in sns.color_palette("Blues_d"):
+		print (255.*np.asarray(i)).astype(int)
+
 if __name__ == '__main__':
-	main()
+	#main()
+	#plot_sinusoid()
+	get_colors()
 
 
 
