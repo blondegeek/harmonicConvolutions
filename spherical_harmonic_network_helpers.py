44104976ec74f156af266857b94fbebbafe751c8
blondegeek <blondegeek@gmail.com>
2017-02-01 21:24:51 -0800

    beginnings of spherical harmonic analogs of harmonic net

---
+++ spherical_harmonic_network_helpers.py
@@ -0,0 +1,60 @@
+import os
+import sys
+import time
+
+import numpy as np
+import tensorflow as tf
+import scipy as sp
+
+from harmonic_network_ops import get_weights
+
+"""
+Helper functions for spherical harmonic (3D) network.
+"""
+
+def get_weights_dict(shape, max_order, std_mult=0.4, name='W', device='/cpu:0'):
+    """Return a dict of weights.
+
+    This function is identical to get_weights_dict in harmonic_network_helpers.py
+    except n_rings and sh is adjusted for 3D. Note, the radial function for a given order (l)
+    is the same for all m's.
+
+    shape: list of filter shape [h,w,d,i,o] --- note we use h=w=d
+    max_order: returns weights for m=0,1,...,max_order
+    std_mult: He init scaled by std_mult (default 0.4)
+    name: (default 'W')
+    dev: (default /cpu:0)
+    """
+    weights_dict = {}
+    radius = (shape[0 ] +1 ) /2
+    n_rings = ( radius * (radius + 1) * (radius + 2) ) / 6
+    for i in xrange( max_order +1):
+        sh = [ n_rings -( i >0)] + shape[3:]
+        nm = name + '_' + str(i)
+        weights_dict[i] = get_weights(sh, std_mult=std_mult, name=nm, device=device)
+    return weights_dict
+
+def get_bias_dict(n_filters, order, name='b', device='/cpu:0'):
+	"""Return a dict of biases"""
+	with tf.device(device):
+		bias_dict = {}
+		for i in xrange(order+1):
+			bias = tf.get_variable(name+'_'+str(i), dtype=tf.float32,
+								   shape=[n_filters],
+				initializer=tf.constant_initializer(1e-2))
+			bias_dict[i] = bias
+	return bias_dict
+
+
+def get_phase_dict(n_in, n_out, order, name='b',device='/cpu:0'):
+	"""Return a dict of phase offsets"""
+	with tf.device(device):
+		phase_dict = {}
+		for i in xrange(order+1):
+			init = np.random.rand(1,1,1,n_in,n_out) * 2. *np.pi
+			init = np.float32(init)
+			phase = tf.get_variable(name+'_'+str(i), dtype=tf.float32,
+									shape=[1,1,1,n_in,n_out],
+				initializer=tf.constant_initializer(init))
+			phase_dict[i] = phase
+	return phase_dict
---
+++ spherical_harmonic_network_ops.py
@@ -0,0 +1,73 @@
+import numpy as np
+import tensorflow as tf
+from sympy.physics.quantum.cg import CG
+
+
+order_lm_dict = {0: (0, 0),
+                 1: (1, -1),
+                 2: (1, 0),
+                 3: (1, 1),
+                 4: (2, -2),
+                 5: (2, -1),
+                 6: (2, 0),
+                 7: (2, 1),
+                 8: (2, 2)}
+
+def conv(X, W, strides, padding, name):
+    """Shorthand for tf.nn.conv3d"""
+    return tf.nn.conv3d(X, W, strides=strides, padding=padding, name=name)
+
+
+def h_conv(X, W, strides=(1, 1, 1, 1), padding='VALID', max_order=1, name='N'):
+    """Inter-order (cross-stream) convolutions can be implemented as single
+    convolutions. For this we store data as 6D tensors and filters as 8D
+    tensors, at convolution, we reshape down to 4D tensors and expand again.
+
+    X: tensor shape [mbatch,h,w,channels,complex,order]
+    Q: tensor dict---reshaped to [h,w,in,in.comp,in.ord,out,out.comp,out.ord]
+    P: tensor dict---phases
+    strides: as per tf convention (default (1,1,1,1))
+    padding: as per tf convention (default VALID)
+    filter_size: (default 3)
+    max_order: (default 1)
+    name: (default N)
+    """
+    with tf.name_scope('hconv' + str(name)) as scope:
+        # Build data tensor
+        Xsh = X.get_shape().as_list()
+        Wsh = W.get_shape().as_list()
+        X_ = tf.reshape(X, tf.concat(0, [Xsh[:3], [-1]]))
+
+        #   Construct the stream-convolutions as one big filter
+        Q_ = []
+        for output_order in xrange(max_order + 1):
+            # For each output order build input
+            Qr = []
+            Qi = []
+            for input_order in xrange(Xsh[3]):
+                for filter_order in xrange(Wsh[0]):
+                    # Compute Clebsch-Gordon coeff
+                    cg = CG(*order_lm_dict[input_order], *order_lm_dict[filter_order], *order_lm_dict[output_order])
+                    cg = cg.doit()
+                    # Need to think about whether CG coeff effects real or imaginary filters like sign for circular...
+                    c = cg * W[filter_order]
+                    # fo = output_order - input_order
+                    # c = W[np.abs(fo)]
+                    #s = np.sign(fo)
+                    # Choose a different filter depending on whether input is real
+                    if Xsh[4] == 2:
+                        # Qr += [c[0], -s * c[1]]
+                        # Qi += [c[1], s * c[0]]
+                        Qr += [c[0], -s * c[1]]
+                        Qi += [c[1], s * c[0]]
+                    else:
+                        Qr += [c[0]]
+                        Qi += [c[1]]
+                # Need to concat to a diff dimension here?
+            Q_ += [tf.concat(2, Qr), tf.concat(2, Qi)]
+        Q_ = tf.concat(3, Q_)
+
+        R = conv(X_, Q_, strides, padding, name + 'cconv')
+        Rsh = R.get_shape().as_list()
+        ns = tf.concat(0, [Rsh[:3], [max_order + 1, 2], [Rsh[3] / (2 * (max_order + 1))]])
+        return tf.reshape(R, ns)
